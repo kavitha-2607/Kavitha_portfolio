0eb7618c1925b88ab2c5e819898840f224795d45
kavitha-2607 <100529648+kavitha-2607@users.noreply.github.com>
2022-04-21 15:46:45 +0530

    Add files via upload

---
+++ Autodbs/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Kavitha Raghunath - Autos Database </title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class = "container">
+<h1> Welcome to Autos Database </h1>
+<p>
+<a href = "login.php"> Please Log In </a>
+</p>
+<p>
+Attempt to go to
+<a href = "autos.php"> autos.php </a>
+</p>
+</div>
+</body>
